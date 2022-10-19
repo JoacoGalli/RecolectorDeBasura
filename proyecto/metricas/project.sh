54a7d02374c0f178e7a4ce6897bade22d2d5ff79
Joaquin Galli <gallijoaquin@protonmail.com>
2022-10-19 17:40:43 -0300

    refactorice los archivos

--- proyecto/metricas/project.sh
+++ proyecto/metricas/project.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 pushd mqtt; docker-compose up -d;popd
-pushd node-red; docker-compose up -d;popd
 pushd influx1; docker-compose up -d;popd
 pushd grafana; docker-compose up -d;popd
